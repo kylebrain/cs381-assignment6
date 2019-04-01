0acd3c8212bcfd10cf67ef57f9e2af93844f230d
Garrett S Moore <garrett22smoore@gmail.com>
2019-04-01 14:12:41 +0000

    removed .

--- Debug/src/Mgrs/Mgr.d
+++
@@ -1,7 +0,0 @@
-src/Mgrs/Mgr.o: ../src/Mgrs/Mgr.cpp \
- /home/garrettmoore/Documents/Assignment6/cs381-assignment6/inc/Engine.h \
- /home/garrettmoore/Documents/Assignment6/cs381-assignment6/inc/Mgrs/Mgr.h
-
-/home/garrettmoore/Documents/Assignment6/cs381-assignment6/inc/Engine.h:
-
-/home/garrettmoore/Documents/Assignment6/cs381-assignment6/inc/Mgrs/Mgr.h:
--- Debug/src/main.d
+++
@@ -1,4 +0,0 @@
-src/main.o: ../src/main.cpp \
- /home/garrettmoore/Documents/Assignment6/cs381-assignment6/inc/Engine.h
-
-/home/garrettmoore/Documents/Assignment6/cs381-assignment6/inc/Engine.h:
